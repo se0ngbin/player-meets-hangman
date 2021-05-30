03ff34b6dcfbc7f413511d1b42606a6377e52683
vanessatu20 <yuwenvanessatu@gmail.com>
2021-05-30 16:47:43 -0700

    update feed UI

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -4,10 +4,16 @@ import Logo from '../assets/logo.png'
 import BellIcon from '../assets/bell_icon.png';
 import UserIcon from '../assets/user_icon.png';
 import "./Homepage.css"
+import Girl1 from '../assets/girl1.jpeg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class Homepage extends Component {
     render() {
+        let userName = "Alexandra";
+        let userAge = "24";
+        let userCity = "Los Angeles";
+        let userState = "California";
         return (
             <div>
                 <div className="navbar">
@@ -15,8 +21,16 @@ class Homepage extends Component {
                     <img src={BellIcon} height="25" className="bellIcon"></img>
                     <Link to="/login"><img src={UserIcon} height="25" className="userIcon"></img></Link>
                 </div>
-                <div>
-                    
+                <div className="body">
+                    <div className="behindLayer1">
+                    </div>
+                    <div className="card">
+                            <img src={Girl1}></img>
+                        </div>
+                    <div className="userInfo">
+                        <div className="title">{userName}, {userAge}</div>
+                        <div className="subtitle">{userCity}, {userState}</div>
+                    </div>
                 </div>
             </div>
         );
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -9,6 +9,7 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./Login.css";
+import Logo from '../assets/logo.png'
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -23,29 +24,34 @@ export default function Login() {
   }
 
   return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-      </Form>
+    <div>
+      <div className="navbar">
+          <img src={Logo} height="30" className="logo"></img>
+      </div>
+      <div className="Login">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group size="lg" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              autoFocus
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group size="lg" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Button block size="lg" type="submit" disabled={!validateForm()}>
+            Login
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
