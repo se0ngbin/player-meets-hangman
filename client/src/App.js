f1aa202557a6544aa167cce70f6eae6e34d4c435
se0ngbin <shannonsbpark@gmail.com>
2021-06-01 19:00:13 +0900

    fixed server error

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios'
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import CreateAccount from "./pages/CreateAccount";
 
+
 class App extends Component {
   state = {
     response: {}
   };
-
-  componentDidMount() {
-    axios.get('/api/v1/say-something').then((res) => {
-      const response = res.data;
-      this.setState({ response });
-    });
-  }
 
   render() {
     return (
@@ -27,8 +20,7 @@ class App extends Component {
             <Route exact path="/login" component={Login} />
             <Route exact path="/createaccount" component={CreateAccount} />
           </Switch>
-        </Router>  
-        <h1>{this.state.response.body}</h1>
+        </Router>
       </div>
     );
   }
